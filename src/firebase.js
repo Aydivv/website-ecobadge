b03dea9e64ed3e4c6e867279ae9e41266229cb8c
Aydiv <55044178+Aydivv@users.noreply.github.com>
2022-02-07 03:29:39 +0000

    Initial setup

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBIEc5dwA6wPlNm3oJkOAL6dmzBIzE1P3o",
+    authDomain: "ecobadge-3d3da.firebaseapp.com",
+    projectId: "ecobadge-3d3da",
+    storageBucket: "ecobadge-3d3da.appspot.com",
+    messagingSenderId: "594209949540",
+    appId: "1:594209949540:web:2a06e9c5072e5ea5d11c71",
+    measurementId: "G-CKQV925LZ5"
+  };
+  
