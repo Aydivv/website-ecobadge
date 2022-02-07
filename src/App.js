3a1ccab156b80e974eaef7fe96a32e9dda30bbf7
Aydiv <55044178+Aydivv@users.noreply.github.com>
2022-02-07 04:56:28 +0000

    Header visuals done.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import Header from "./Header";
+import React from "react";
 
 function App() {
   return (
     <div className="app">
-      <h1>sup lol</h1>
       {/* Header */}
       <Header />
       {/* Home */}
--- src/Header.js
+++ src/Header.js
@@ -1,25 +1,26 @@
 import React from "react";
 import "./Header.css";
+import navLogo from "./img/logonav.png";
 
 function Header() {
   return (
     <div className="header">
-      <img className="header__logo" src="./img/logonav.png" />
+      <img className="header__logo" src={navLogo} alt="EcoBadge logo" />
       <div className="header__nav">
         <div className="header__option">
-          <span className="header__optionLineOne">Home</span>
+          <span className="header__optionText">Home</span>
         </div>
         <div className="header__option">
-          <span className="header__optionLineOne">Directory</span>
+          <span className="header__optionText">Directory</span>
         </div>
         <div className="header__option">
-          <span className="header__optionLineOne">About Us</span>
+          <span className="header__optionText">About Us</span>
         </div>
         <div className="header__option">
-          <span className="header__optionLineOne">Contact Us</span>
+          <span className="header__optionText">Contact Us</span>
         </div>
         <div className="header__option">
-          <span className="header__optionLineOne">Sign Up</span>
+          <span className="header__optionText">Sign Up</span>
         </div>
       </div>
     </div>
