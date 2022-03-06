1dcc06c0fa69caee3ce1e96b8b9da8bde3421c77
Aydiv <55044178+Aydivv@users.noreply.github.com>
2022-03-06 20:02:04 +0000

    Tailwind installed

--- src/Header.js
+++ src/Header.js
@@ -1,28 +1,12 @@
 import React from "react";
-import "./Header.css";
 import navLogo from "./img/logonav.png";
 
 function Header() {
   return (
-    <div className="header">
-      <img className="header__logo" src={navLogo} alt="EcoBadge logo" />
-      <div className="header__nav">
-        <div className="header__option">
-          <span className="header__optionText">Home</span>
-        </div>
-        <div className="header__option">
-          <span className="header__optionText">Directory</span>
-        </div>
-        <div className="header__option">
-          <span className="header__optionText">About Us</span>
-        </div>
-        <div className="header__option">
-          <span className="header__optionText">Contact Us</span>
-        </div>
-        <div className="header__option">
-          <span className="header__optionText">Sign Up</span>
-        </div>
-      </div>
+    <div className="flex lg:justify-center justify-end py-4 px-2">
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
     </div>
   );
 }
